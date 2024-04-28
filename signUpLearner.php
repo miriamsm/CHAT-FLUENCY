2f3e46814b296375fb498fad4efa11ede0284f0d
reef-alqadi <reef.alqadi@gmail.com>
2024-04-28 16:33:23 +0300

    a

--- signUpLearner.php
+++ signUpLearner.php
@@ -57,7 +57,7 @@ if(isset($_POST['submit'])){
    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
    <title>Sign up</title> 
  
-   <!-- font awesome cdn link  --> 
+   <!-- font awesome cyyydn link  --> 
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"> 
  
    <!-- custom css file link  --> 
