c0aa908237f652cd98d80aff455e8f5f01e8bee1
Miriam <miriamoffic@gmail.com>
2024-05-06 22:04:24 +0300

    update database

--- chatfluency.sql
+++ chatfluency.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 30, 2022 at 05:35 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Generation Time: May 02, 2024 at 04:41 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,82 +24,216 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `bookmark`
+-- Table structure for table `languagelearners`
 --
 
--- Table for Language Learners
-CREATE TABLE LanguageLearners (
-    LearnerID INT PRIMARY KEY AUTO_INCREMENT,
-    FirstName VARCHAR(15) NOT NULL,
-    LastName VARCHAR(15) NOT NULL,
-    Email VARCHAR(30) UNIQUE NOT NULL,
-    Password VARCHAR(50) NOT NULL,
-    Photo VARCHAR(255), -- File path to optional photo
-    City VARCHAR(20),
-    Location VARCHAR(100),
-    User_Role ENUM('learner', 'partner') DEFAULT 'learner',
-    -- SignUpDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    -- LastLogin TIMESTAMP,
-    -- Ensuring unique combination of first and last name
-);
+CREATE TABLE `languagelearners` (
+  `LearnerID` int(11) NOT NULL,
+  `FirstName` varchar(15) NOT NULL,
+  `LastName` varchar(15) NOT NULL,
+  `Email` varchar(30) NOT NULL,
+  `Password` varchar(50) NOT NULL,
+  `Photo` varchar(255) DEFAULT NULL,
+  `City` varchar(20) DEFAULT NULL,
+  `Location` varchar(100) DEFAULT NULL,
+  `User_Role` enum('learner','partner') NOT NULL DEFAULT 'learner'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Table for Language Learning Requests
-CREATE TABLE LearningRequests (
-    RequestID INT PRIMARY KEY AUTO_INCREMENT,
-    LearnerID INT,
-    LanguageToLearn VARCHAR(15) NOT NULL,
-    ProficiencyLevel VARCHAR(15),
-    PreferredSchedule VARCHAR(50),
-    SessionDuration INT,
-    RequestDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    Status ENUM('Pending', 'Accepted', 'Rejected') DEFAULT 'Pending',
-    FOREIGN KEY (LearnerID) REFERENCES LanguageLearners(LearnerID) ON DELETE CASCADE
-);
+--
+-- Dumping data for table `languagelearners`
+--
 
--- Table for Language Partners/Native Speakers
-CREATE TABLE LanguagePartners (
-    PartnerID INT PRIMARY KEY AUTO_INCREMENT,
-    FirstName VARCHAR(15) NOT NULL,
-    LastName VARCHAR(15) NOT NULL,
-    Age INT,
-    Gender ENUM('Male', 'Female'),
-    Email VARCHAR(100) UNIQUE NOT NULL,
-    Photo VARCHAR(255), -- File path to optional photo
-    Password VARCHAR(50) NOT NULL,
-    Phone VARCHAR(15),
-    City VARCHAR(20),
-    Bio TEXT,
-    User_Role ENUM('learner', 'partner') DEFAULT 'partner',
-    Languages TEXT, 
-    Rating Float,
-    -- SignUpDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    -- LastLogin TIMESTAMP,
-    -- Ensuring unique combination of first and last name
-);
+INSERT INTO `languagelearners` (`LearnerID`, `FirstName`, `LastName`, `Email`, `Password`, `Photo`, `City`, `Location`, `User_Role`) VALUES
+(123456789, 'Leena', 'Alshaikh', 'Leena@gmail.com', '12345L', NULL, 'Riyadh', 'KSA', 'learner');
 
--- Table for Learning Sessions
-CREATE TABLE LearningSessions (
-    SessionID INT PRIMARY KEY AUTO_INCREMENT,
-    LearnerID INT,
-    PartnerID INT,
-    SessionDate DATETIME,
-    SessionDuration INT,
-    Status ENUM('Scheduled', 'Completed', 'Canceled') DEFAULT 'Scheduled',
-    FOREIGN KEY (LearnerID) REFERENCES LanguageLearners(LearnerID) ON DELETE CASCADE,
-    FOREIGN KEY (PartnerID) REFERENCES LanguagePartners(PartnerID) ON DELETE CASCADE
-);
+-- --------------------------------------------------------
 
--- Table for Reviews and Ratings
-CREATE TABLE ReviewsRatings (
-    ReviewID INT PRIMARY KEY AUTO_INCREMENT,
-    SessionID INT,
-    Rating INT,
-    ReviewText TEXT,
-    ReviewDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (SessionID) REFERENCES LearningSessions(SessionID) ON DELETE CASCADE
-);
+--
+-- Table structure for table `languagepartners`
+--
 
+CREATE TABLE `languagepartners` (
+  `PartnerID` int(11) NOT NULL,
+  `FirstName` varchar(15) NOT NULL,
+  `LastName` varchar(15) NOT NULL,
+  `Age` int(11) DEFAULT NULL,
+  `Gender` enum('Male','Female') DEFAULT NULL,
+  `Email` varchar(100) NOT NULL,
+  `Photo` varchar(255) DEFAULT NULL,
+  `Password` varchar(50) NOT NULL,
+  `Phone` varchar(15) DEFAULT NULL,
+  `City` varchar(20) DEFAULT NULL,
+  `Bio` text DEFAULT NULL,
+  `User_Role` enum('learner','partner') NOT NULL DEFAULT 'partner',
+  `Languages` text NOT NULL,
+  `Rating` float NOT NULL,
+  `LanguageProf` text NOT NULL,
+  `SessionPrice` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `languagepartners`
+--
+
+INSERT INTO `languagepartners` (`PartnerID`, `FirstName`, `LastName`, `Age`, `Gender`, `Email`, `Photo`, `Password`, `Phone`, `City`, `Bio`, `User_Role`, `Languages`, `Rating`, `LanguageProf`, `SessionPrice`) VALUES
+(12, 'John', 'deo', 34, 'Male', 'JohnDeo@hotmail.com', 'Null', '12345J', '0504168373', 'Riyadh', 'Hey i\'m john', 'partner', 'Spanish', 4.9, 'Proff', 0),
+(123456789, 'Alanood', 'Ibrahim', 21, 'Female', 'alanoud.ibrahim5@gmail.com', 'Null', '12345A', '0592744070', 'Riyadh', 'Hello, I\'m Alanood', 'partner', 'English, Arabic', 4.5, 'Beginner', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `learningrequests`
+--
+
+CREATE TABLE `learningrequests` (
+  `RequestID` int(11) NOT NULL,
+  `LearnerID` int(11) DEFAULT NULL,
+  `PartnerID` int(11) NOT NULL,
+  `LanguageToLearn` set('English','Spanish','French','Mandarian Chinese','Arabic','Hindi','Russian','Portuguese','Bengali','German') NOT NULL,
+  `ProficiencyLevel` set('Beginner','Intermediate','Advanced','') DEFAULT NULL,
+  `PreferredSchedule` varchar(50) DEFAULT NULL,
+  `SessionDuration` varchar(15) DEFAULT NULL,
+  `RequestDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `Status` enum('Pending','Accepted','Rejected') DEFAULT 'Pending',
+  `LearnerGoals` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `learningrequests`
+--
+
+INSERT INTO `learningrequests` (`RequestID`, `LearnerID`, `PartnerID`, `LanguageToLearn`, `ProficiencyLevel`, `PreferredSchedule`, `SessionDuration`, `RequestDate`, `Status`, `LearnerGoals`) VALUES
+(1, 123456789, 12, 'English', 'Intermediate', 'Weekend', '2', '2024-04-30 22:35:55', 'Pending', 'My goals is....');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `learningsessions`
+--
+
+CREATE TABLE `learningsessions` (
+  `SessionID` int(11) NOT NULL,
+  `LearnerID` int(11) DEFAULT NULL,
+  `PartnerID` int(11) DEFAULT NULL,
+  `SessionDate` datetime DEFAULT NULL,
+  `SessionDuration` int(11) DEFAULT NULL,
+  `Status` enum('Scheduled','Completed','Canceled') DEFAULT 'Scheduled'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviewsratings`
+--
+
+CREATE TABLE `reviewsratings` (
+  `ReviewID` int(11) NOT NULL,
+  `SessionID` int(11) DEFAULT NULL,
+  `Rating` int(11) DEFAULT NULL,
+  `ReviewText` text DEFAULT NULL,
+  `ReviewDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `PartnerID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `languagelearners`
+--
+ALTER TABLE `languagelearners`
+  ADD PRIMARY KEY (`LearnerID`),
+  ADD UNIQUE KEY `Email` (`Email`);
+
+--
+-- Indexes for table `languagepartners`
+--
+ALTER TABLE `languagepartners`
+  ADD PRIMARY KEY (`PartnerID`),
+  ADD UNIQUE KEY `Email` (`Email`);
+
+--
+-- Indexes for table `learningrequests`
+--
+ALTER TABLE `learningrequests`
+  ADD PRIMARY KEY (`RequestID`),
+  ADD KEY `LearnerID` (`LearnerID`),
+  ADD KEY `PartnerID` (`PartnerID`);
+
+--
+-- Indexes for table `learningsessions`
+--
+ALTER TABLE `learningsessions`
+  ADD PRIMARY KEY (`SessionID`),
+  ADD KEY `LearnerID` (`LearnerID`),
+  ADD KEY `PartnerID` (`PartnerID`);
+
+--
+-- Indexes for table `reviewsratings`
+--
+ALTER TABLE `reviewsratings`
+  ADD PRIMARY KEY (`ReviewID`),
+  ADD KEY `SessionID` (`SessionID`),
+  ADD KEY `PartnerID` (`PartnerID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `languagelearners`
+--
+ALTER TABLE `languagelearners`
+  MODIFY `LearnerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=123456790;
+
+--
+-- AUTO_INCREMENT for table `languagepartners`
+--
+ALTER TABLE `languagepartners`
+  MODIFY `PartnerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=123456790;
+
+--
+-- AUTO_INCREMENT for table `learningrequests`
+--
+ALTER TABLE `learningrequests`
+  MODIFY `RequestID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `learningsessions`
+--
+ALTER TABLE `learningsessions`
+  MODIFY `SessionID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `reviewsratings`
+--
+ALTER TABLE `reviewsratings`
+  MODIFY `ReviewID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `learningrequests`
+--
+ALTER TABLE `learningrequests`
+  ADD CONSTRAINT `learningrequests_ibfk_1` FOREIGN KEY (`LearnerID`) REFERENCES `languagelearners` (`LearnerID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `learningsessions`
+--
+ALTER TABLE `learningsessions`
+  ADD CONSTRAINT `learningsessions_ibfk_1` FOREIGN KEY (`LearnerID`) REFERENCES `languagelearners` (`LearnerID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `learningsessions_ibfk_2` FOREIGN KEY (`PartnerID`) REFERENCES `languagepartners` (`PartnerID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `reviewsratings`
+--
+ALTER TABLE `reviewsratings`
+  ADD CONSTRAINT `reviewsratings_ibfk_1` FOREIGN KEY (`SessionID`) REFERENCES `learningsessions` (`SessionID`) ON DELETE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
