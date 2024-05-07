27219dcac066055f68dee94494cb9acd806e6647
reef-alqadi <reef.alqadi@gmail.com>
2024-05-07 17:32:06 +0300

    j

--- signUpPartner.php
+++ signUpPartner.php
@@ -22,49 +22,51 @@ if (isset($_POST['submit'])) {
     $spokenLanguage = $_POST['spoken_language'];
     $shortBio = $_POST['short_bio'];
  // Handle file upload
- $photo = $_FILES['photo']['name'];
- $target_dir = 'images/';
- $target_file = $target_dir . basename($_FILES["photo"]["name"]);
- $uploadOk = 1;
- $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+if(isset($_FILES['photo']['name']) && !empty($_FILES['photo']['name'])) {
+   // File was uploaded by the user
+   $photo = $_FILES['photo']['name'];
+   $target_dir = 'images/';
+   $target_file = $target_dir . basename($_FILES["photo"]["name"]);
+   $uploadOk = 1;
+   $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
- // Check if image file is a actual image or fake image
- if(isset($_POST["submit"])) {
-     $check = getimagesize($_FILES["photo"]["tmp_name"]);
-     if($check !== false) {
-         $uploadOk = 1;
-     } else {
-         echo "File is not an image.";
-         $uploadOk = 0;
-     }
- }
+   // Check if image file is a actual image or fake image
+   $check = getimagesize($_FILES["photo"]["tmp_name"]);
+   if($check === false) {
+       echo "File is not an image.";
+       $uploadOk = 0;
+   }
 
- // Check file size
- if ($_FILES["photo"]["size"] > 500000) {
-     echo "Sorry, your file is too large.";
-     $uploadOk = 0;
- }
+   // Check file size
+   if ($_FILES["photo"]["size"] > 500000) {
+       echo "Sorry, your file is too large.";
+       $uploadOk = 0;
+   }
 
- // Allow certain file formats
- if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
- && $imageFileType != "gif" ) {
-     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-     $uploadOk = 0;
- }
+   // Allow certain file formats
+   if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+   && $imageFileType != "gif" ) {
+       echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+       $uploadOk = 0;
+   }
 
- // Check if $uploadOk is set to 0 by an error
- if ($uploadOk == 0) {
-     echo "Sorry, your file was not uploaded.";
- // if everything is ok, try to upload file
- } else {
-     if (move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file)) {
-         echo "The file ". basename( $_FILES["photo"]["name"]). " has been uploaded.";
-         //$photo = $target_file;
-     } else {
-         echo "Sorry, there was an error uploading your file.";
-     }
- }
-
+   // Check if $uploadOk is set to 0 by an error
+   if ($uploadOk == 0) {
+       echo "Sorry, your file was not uploaded.";
+       // Set default photo
+       $photo = 'Profile.png';
+   } else {
+       // Move uploaded file to desired location
+       if (move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file)) {
+           echo "The file ". basename( $_FILES["photo"]["name"]). " has been uploaded.";
+       } else {
+           echo "Sorry, there was an error uploading your file.";
+       }
+   }
+} else {
+   // No file was uploaded, set default photo
+   $photo = 'Profile.png';
+}
     // SQL query to insert data into languagepartners table
     $query = "INSERT INTO languagepartners (FirstName, LastName, Age, Gender, Email, Password, Phone, City, Bio, Languages, Photo) 
     VALUES ('$firstName', '$lastName', '$age', '$gender', '$email', '$password', '$phone', '$city', '$shortBio', '$spokenLanguage', '$photo')";
